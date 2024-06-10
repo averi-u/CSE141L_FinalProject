6eafc806bf706fcb3cd50403bd7ede685385371e
Averi Yu / Zhizhen Yu <73741014+averi-u@users.noreply.github.com>
2024-06-09 17:57:07 -0700

    c

--- ISA_demo/JLUT.sv
+++ ISA_demo/JLUT.sv
@@ -1,14 +1,30 @@
+module JLUT (
+    input [5:0] address,
+    output reg [5:0] jumpAddress
+);
 
+    reg [5:0] jumpTable [0:63]; // 64 x 6-bit jump address table
 
-module JLUT(
-  input[1:0] Jptr,
-  output logic[5:0] Jump);
+    initial begin
+        // Initialize the jump table
+        $readmemb("jump_table.txt", jumpTable);
+    end
 
-  always_comb case(Jptr)
-	0: Jump = 5; 
-	1: Jump = 3;
-	2: Jump = 6;
-	3: Jump = 8;
-  endcase
-
+    always @(*) begin
+        jumpAddress <= jumpTable[address];
+    end
 endmodule
+
+
+// module JLUT(
+//   input[1:0] Jptr,
+//   output logic[5:0] Jump);
+
+//   always_comb case(Jptr)
+// 	0: Jump = 5; 
+// 	1: Jump = 3;
+// 	2: Jump = 6;
+// 	3: Jump = 8;
+//   endcase
+
+// endmodule
--- ISA_demo/ProgCtr.sv
+++ ISA_demo/ProgCtr.sv
@@ -1,13 +1,33 @@
-module ProgCtr(
-  input             Clk,
-                    Reset,
-					Jen,
-  input       [5:0] Jump,
-  output logic[5:0] PC);
+module ProgCtr (
+    input clk,
+    input reset,
+    input [5:0] nextPC,
+    input jump,
+    output reg [5:0] pc
+);
 
-  always_ff @(posedge Clk)
-    if(Reset) PC <= 'b0;
-	else if(Jen) PC <= Jump;
-	else      PC <= PC + 6'd1;
-
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            pc <= 0;
+        end else if (jump) begin
+            pc <= nextPC;
+        end else begin
+            pc <= pc + 1;
+        end
+    end
 endmodule
+
+
+// module ProgCtr(
+//   input             Clk,
+//                     Reset,
+// 					Jen,
+//   input       [5:0] Jump,
+//   output logic[5:0] PC);
+
+//   always_ff @(posedge Clk)
+//     if(Reset) PC <= 'b0;
+// 	else if(Jen) PC <= Jump;
+// 	else      PC <= PC + 6'd1;
+
+// endmodule
